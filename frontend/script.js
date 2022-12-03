057f221a98505d3b51b49a850634994855920a81
Rafal - Master of the Universe <r.michniuk@gmail.com>
2022-12-03 16:56:00 +0100

    alternative solutions

--- frontend/script.js
+++ frontend/script.js
@@ -6,3 +6,31 @@
 //     - it can only contains letters, digits and hyphens
 //   2. The validation should occur when a user leaves the input
 //   3. It should not be possible to submit a form until the input has a valid value
+
+const formName = document.getElementById('name');
+const submitBtn = document.getElementById('btnForm');
+const restrictions = /^[-a-zA-Z0-9]+$/;
+
+function formValidator(e) {
+  if (formName.value.length <= 3 || formName.value.length >= 21) {
+    e.preventDefault();
+    alert(
+      'Your input is invalid. Usernames must contain at least 4 characters and 20 at maximum and can only contains letters, digits and hyphens'
+    );
+    return;
+  }
+
+  if (!formName.value.match(restrictions)) {
+    e.preventDefault();
+    alert('You tried submitting although the input is still invalid');
+    return;
+  }
+}
+
+formName.addEventListener('blur', (e) => {
+  formValidator(e);
+});
+
+submitBtn.addEventListener('click', (e) => {
+  formValidator(e);
+});
