b0ff8456378aa567d2f628425601bd15f5a3610f
Kevinfd12 <kevinfd12@gmail.com>
2022-12-26 15:52:21 -0500

    Homework 10 answer

--- 12_3_22/10.js
+++ 12_3_22/10.js
@@ -1,3 +1,26 @@
 // array-6
 // // Write a JavaScript program which accept a number as input and insert dashes (-) between each two even numbers.
 // For example if you accept 025468 the output should be 0-254-6-8.
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+function insertDashes(num) {
+  let output = '';
+  let numString = num.toString();
+
+  for (let i = 0; i < numString.length; i++) {
+    const curr = numString[i];
+    const nextNum = numString[i + 1];
+    if (isEven(curr) && isEven(nextNum)) {
+      output = output + curr + '-';
+    } else {
+      output = output + curr;
+    }
+  }
+  return output;
+}
+
+const result = insertDashes(025466);
+console.log(result);
